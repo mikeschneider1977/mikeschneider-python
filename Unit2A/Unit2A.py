cb6593af7aac90b8a8879b76d43788306e640688
mike schneider <mikehschneider@gmail.com>
2018-09-06 15:37:10 -0700

    lab for unit2A

---
+++ Unit2A/Unit2A.py
@@ -0,0 +1,22 @@
+i = 100
+j = 200
+x = 300
+y = 100
+
+print(5 + int(5.5))
+
+print((i == j) or (i > x))
+
+print((i < j) and (i >= y))
+
+print((i != j) and (i != x))
+
+print((i < j) and (i < x) and (i < y))
+
+## myNumber = input()
+
+## print(int(myNumber) + 5)
+
+
+
+
