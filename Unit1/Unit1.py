6d7324299dc77a75b219ae3b52b96ae38775ed30
mike schneider <mikehschneider@gmail.com>
2018-09-06 15:44:06 -0700

    unit1 lab

---
+++ Unit1/Unit1.py
@@ -0,0 +1,18 @@
+print('i love python')
+
+myName = input()
+
+print(myName)
+
+print(5 + 5)
+
+print(5 * 5)
+
+print(12 / 5)
+
+print(12 % 5)
+
+myNumber1 = input()
+myNumber2 = input()
+
+print(myNumber1 + myNumber2)
