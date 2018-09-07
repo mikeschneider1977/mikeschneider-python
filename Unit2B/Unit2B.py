a52ce358e939c55b1b632f31be232944829fd21e
mike schneider <mikehschneider@gmail.com>
2018-09-06 21:46:15 -0700

    laptop

---
+++ Unit2B/Unit2B.py
@@ -0,0 +1,20 @@
+myList = [1,2,3,4]
+myList2 = ['a', 'b', 'c', 'd']
+
+print(myList)
+
+print(myList[0])
+
+myList.insert(0,-1)
+
+print(myList)
+
+myList.insert(3, 100)
+
+print(myList)
+
+myList.insert(3, int(input()))
+
+print(myList)
+print(myList2)
+print(myList2[3])
