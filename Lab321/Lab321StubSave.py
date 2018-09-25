a80dd08dd92c3be2235b78ad9bf13269fc1a42c7
mike schneider <mikehschneider@gmail.com>
2018-09-25 10:25:21 -0700

    laptop

---
+++ Lab321/Lab321StubSave.py
@@ -0,0 +1,49 @@
+def main():
+    print('start here')
+    ## pirnt title
+
+    ## ask for input
+
+    ## call getYearInSchool function with input as argument
+    ## 1 argument gets passed to getYearInASchool
+    getYearInSchool()
+
+    ## print long grade
+
+    ## create list of 4 to 6 grades
+    ## call calcaAverageGrade function with list and number in list as arguments
+    ## 2 arguments get passed to calcAverageGrade
+    calcAverageGrade()
+
+    ## print average grade
+
+    ## call getLetterGrae function with average grade as argument
+    ## 1 argument gets passed to getLetterGrade
+    getLetterGrade()
+
+    ## print letter grade
+
+    ## determiinem if letter grade is passing or failing and print
+
+
+
+def getYearInSchool():  ## 1 argument
+    print('getYearInSchool')
+    ## get long grade and return long grade to main
+
+
+
+def calcAverageGrade(): ## 2 arguments
+    print('calcAverageGrade')
+    ## calculate average grade  and return value to main
+
+
+def getLetterGrade():  ## 1 argument
+    print('getLetterGrade')
+    ## get letter grade and return letter grade to main
+
+
+
+
+
+main()
